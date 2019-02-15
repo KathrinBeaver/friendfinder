5b6f4a2e82fbcdbb8ea91c75409884a7d60e5bf2
Ольга Крылова <>
2019-02-15 03:14:49 +0300

    TEST

---
+++ src/main/java/com/friendsfinder/finder/Morfology.java
@@ -0,0 +1,37 @@
+package com.friendsfinder.finder;
+
+import jmorfsdk.JMorfSdk;
+import jmorfsdk.load.JMorfSdkLoad;
+import morphological.structures.internal.OmoForm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Morfology {
+        public Morfology()
+        {
+        }
+
+        public static JMorfSdk SDK = JMorfSdkLoad.loadFullLibrary();;
+
+        public static ArrayList<String> getForms(String word) throws Exception {
+            ArrayList<String> array = new ArrayList<String>();
+
+            List<OmoForm> characteristics = SDK.getAllCharacteristicsOfForm(word);
+            characteristics.forEach((form) -> {
+
+                if(!array.contains(form.getInitialFormString())) {
+                    array.add(form.getInitialFormString());
+                }
+            });
+            if(array.size()==0) array.add(word);
+            return array;
+        }
+
+        public static void main(String[] args) throws Exception {
+
+            ArrayList<String> array = getForms("маме");
+        }
+    }
+
+
